5f29eeed150a0683f9d6c5dc78f4393ca3c37121
Trey <131633485+doctatrey@users.noreply.github.com>
2026-01-28 22:20:44 -0800

    deleting

--- Personal Projects/string reverse/reverse.js
+++
@@ -1,7 +0,0 @@
-var originalString = document.getElementById('string');
-var newString = document.getElementById('result');
-const reverseButton = document.getElementById('reverseButton');
-
-reverseButton.addEventListener('click', () => {
-    newString.innerHTML = originalString.value.split('').reverse().join('');
-});
